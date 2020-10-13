b2420de2d8010ec1e40624ed0427b4823c5585f0
Romualdo Santiago Santiago <malbe_11@hotmail.com>
2020-10-13 16:16:11 -0500

    creacion de las tablas para almacenar los libros y sus categorias

---
+++ database/migrations/2020_10_13_200950_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('author');
+            $table->foreignId('category_id');
+            $table->date('published');
+            $table->foreignId('user_id')->comment('person that borrowed a book');
+            $table->boolean('available')->comment('check if book is available or is borrowed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
